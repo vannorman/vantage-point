5b5b9f6ec4f9bdcd05f64895f6b0cdc13f0ee720
Charles Van Norman <ccvannorman@gmail.com>
2017-09-19 15:36:16 -0700

    hompeage

--- hotkey_tv/views.py
+++ hotkey_tv/views.py
@@ -28,56 +28,7 @@ def simple_page(template):
 
 def home(request):
 	obj = {}
-	obj['portfolio'] = []
-	obj['portfolio'].append({
-		"title" : "Super Math World",
-		"link" : "https://supermathworld.com",
-		"year" : "2016 - 2017",
-		"position" : "Founder, CTO",
-		"subtitle" : "A WebGL based math sandbox",
-		"description" : "I built a content generation platform on top of an educational math game. This allows users to edit, create, and share content from directly inside the application, natively in a web browser.",
-		"responsibilities" : 
-		[
-				"Framework architecture",
-				"Game design",
-				"Web backend using python/django/SQL",
-				"SysAdmin using AWS EC2, Route53, SSL, S3, and Cloudfront",
-				"Payment system using Stripe",
-		],
-		"videos" : [
-				{ "source": "https://player.vimeo.com/video/219464062", "image" : "smw_1.jpg" }
-		],
-		"images" : ["smw_2.jpg","smw_4.jpg","smw_3.jpg"],	
-		})
-	obj['portfolio'].append({
-		"title" : "Mathbreakers",
-		"link" : "https://mathbreakers.com",
-		"year" : "2013 - 2015",
-		"position" : "Co-founder",
-		"subtitle" : "A math puzzle platformer for grades 2 - 8",
-		"description" : "We partnered with some of the biggest names in math education, including Dan Meyer and Jo Boaler, to discover the intersection between 3-D action gaming and elementary mathematics. The result was a truly immersive, stress free math game that kids love to play.",
-		"responsibilities" : ["Game design","Game development","Playtesting","Educational content","Business development"],
-		"videos" : [
-				{ "source": "https://player.vimeo.com/video/73754523", "image" : "mb_1.jpg" }
-		],
-		"images" : ["mb_2.jpg","mb_3.jpg","mb_4.jpg"],	
-		})
-	obj['portfolio'].append({
-		"title" : "Mouse Brain Explorer",
-		"link" : "https://3scan.com",
-		"year" : "2015",
-		"position" : "Contractor",
-		"subtitle" : "Fly through a real mouse brain",
-		"description" : "Using a cubic centimeter of a mouse brain imaged with 3Scan's equipment, I made a virtual reality tour through the vasculature and a mini-game to destroy blood clots. This was on exhibit during the Exploratorium's science week in 2015.",
-		"responsibilities" : ["Simulation developer","Oculus, PC, and XBox controller setup"],
-		"videos" : [
-				{ "source": "https://player.vimeo.com/video/117482417", "image" : "3scan_1.jpg" }
-		],
-		"images" : [],	
-		})
-
-
-
+	obj['threeCanvas'] = {} # hmm
 	return renderWithNav(request,'home.html', obj)
 
 def file_a(request):
